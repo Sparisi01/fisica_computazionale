e0e126a0194dcd2e6605c27af7542968bfb577af
Sparisi01 <sparisi2002@gmail.com>
2024-03-14 11:55:26 +0100

    implementazione mk4

--- Esercitazione3/metodiEqdifferenziali.c
+++ Esercitazione3/metodiEqdifferenziali.c
@@ -2,32 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct vec3 {
+struct vecst {
     double tempo;
     double posizione;
     double velocita;
-} vec3;
+};
 
-double H_oscillatore(vec3 q) {
+typedef struct vecst vecst;
+
+double
+H_oscillatore(vecst q) {
     return 0.5 * (pow(q.posizione, 2) + pow(q.velocita, 2));
 }
 
-int euleroEsplicito(vec3* q, double t, int N, vec3 (*F)(vec3, double*), double* args, FILE* traiettoria) {
+int euleroEsplicito(vecst* q, double t, int N, vecst (*F)(vecst, double*), double* args, FILE* traiettoria) {
     double h = (t - q->tempo) / N;
     for (size_t i = 0; i < N; i++) {
-        *q = (vec3){
+        *q = (vecst){
             q->tempo + h,
             q->posizione + h * F(*q, args).velocita,
             q->velocita + h * F(*q, args).posizione,
         };
-        fprintf(traiettoria, "%10.5e %10.5e %10.5e %10.5e\n", q->tempo, q->posizione, q->velocita, H_oscillatore(*q));
+        fprintf(traiettoria, "%10.5E %10.5E %10.5E %10.5E\n", q->tempo, q->posizione, q->velocita, H_oscillatore(*q));
     }
 
     return 0;
 }
 
-int euleroEsplicitoStep(vec3* q, double h, vec3 (*F)(vec3, double*), double* args) {
-    *q = (vec3){
+int euleroEsplicitoStep(vecst* q, double h, vecst (*F)(vecst, double*), double* args) {
+    *q = (vecst){
         q->tempo + h,
         q->posizione + h * F(*q, args).velocita,
         q->velocita + h * F(*q, args).posizione,
@@ -35,40 +38,68 @@ int euleroEsplicitoStep(vec3* q, double h, vec3 (*F)(vec3, double*), double* arg
     return 1;
 }
 
-int euleroImplicitoOscillatore(vec3* q, double t, int N, FILE* traiettoria) {
+int euleroImplicitoOscillatore(vecst* q, double t, int N, FILE* traiettoria) {
     double h = (t - q->tempo) / N;
     if (h < 1e-8)
         return 0;
     for (size_t i = 0; i < N; i++) {
-        *q = (vec3){
+        *q = (vecst){
             q->tempo + h,
             (q->posizione + h * q->velocita) / (1 + h * h),
             (q->velocita - h * q->posizione) / (1 + h * h),
         };
-        fprintf(traiettoria, "%10.5e %10.5e %10.5e %10.5e\n", q->tempo, q->posizione, q->velocita, H_oscillatore(*q));
+        fprintf(traiettoria, "%10.5E %10.5E %10.5E %10.5E\n", q->tempo, q->posizione, q->velocita, H_oscillatore(*q));
     }
 
     return 1;
 }
 
-int euleroTrapeziOscillatore(vec3* q, double t, int N, FILE* traiettoria) {
+int euleroTrapeziOscillatore(vecst* q, double t, int N, FILE* traiettoria) {
     double h = (t - q->tempo) / N;
     if (h < 1e-8)
         return 0;
     for (size_t i = 0; i < N; i++) {
-        *q = (vec3){
+        *q = (vecst){
             q->tempo + h,
             q->posizione * (1 - h * h) + h * q->velocita,
             q->velocita - h * q->posizione,
         };
-        fprintf(traiettoria, "%10.5e %10.5e %10.5e %10.5e\n", q->tempo, q->posizione, q->velocita, H_oscillatore(*q));
+        fprintf(traiettoria, "%10.5E %10.5E %10.5E %10.5E\n", q->tempo, q->posizione, q->velocita, H_oscillatore(*q));
     }
 
     return 1;
 }
 
-vec3 F(vec3 q, double* parametri) {
-    return (vec3){
+int mk4(vecst* q, double h, vecst (*F)(vecst, double*), double* args) {
+    double k1, k2, k3, k4, l1, l2, l3, l4;
+    vecst tempF;
+
+    k1 = h * F(*q, args).posizione;
+    l1 = h * F(*q, args).velocita;
+
+    tempF = F((vecst){q->tempo + h / 2, q->posizione + k1 / 2, q->velocita + l1 / 2}, args);
+    k2 = h * tempF.posizione;
+    l2 = h * tempF.velocita;
+
+    tempF = F((vecst){q->tempo + h / 2, q->posizione + k2 / 2, q->velocita + l2 / 2}, args);
+    k3 = h * tempF.posizione;
+    l3 = h * tempF.velocita;
+
+    tempF = F((vecst){q->tempo + h, q->posizione + k3, q->velocita + l3}, args);
+    k4 = h * tempF.posizione;
+    l4 = h * tempF.velocita;
+
+    *q = (vecst){
+        q->tempo + h,
+        q->posizione + 1 / 6 * (k1 + 2 * k2 + 2 * k3 + k4),
+        q->velocita + 1 / 6 * (l1 + 2 * l2 + 2 * l3 + l4),
+    };
+
+    return 1;
+}
+
+vecst F(vecst q, double* parametri) {
+    return (vecst){
         q.tempo,
         -q.posizione,
         q.velocita,
@@ -79,20 +110,29 @@ int main(int argc, char const* argv[]) {
     FILE* plot_E_implicito = fopen("plot_E_implicito.dat", "w");
     FILE* plot_Trapezio = fopen("plot_Trapezio.dat", "w");
     FILE* plot_E_esplicito = fopen("plot_E_esplicito.dat", "w");
+    FILE* plot_E_mk4 = fopen("plot_E_mk4.dat", "w");
 
-    vec3* vector1 = &(vec3){0, 1, 0};
-    vec3* vector2 = &(vec3){0, 1, 0};
-    vec3* vector3 = &(vec3){0, 1, 0};
+    vecst* vector1 = &(vecst){0, 1, 0};
+    vecst* vector2 = &(vecst){0, 1, 0};
+    vecst* vector3 = &(vecst){0, 1, 0};
+    vecst* vector4 = &(vecst){0, 1, 0};
 
-    // euleroEsplicito(vector1, 250, 10000, F, NULL, plot_E_esplicito);
-    double h = 250.f / 10000.f;
-    for (size_t i = 0; i < 10000; i++) {
+    double T = 25;
+    int N = 1000;
+
+    double h = T / N;
+    for (size_t i = 0; i < N; i++) {
         euleroEsplicitoStep(vector1, h, F, NULL);
-        fprintf(plot_E_esplicito, "%10.5e %10.5e %10.5e %10.5e\n", vector1->tempo, vector1->posizione, vector1->velocita, H_oscillatore(*vector1));
+        fprintf(plot_E_esplicito, "%10.5E %10.5E %10.5E %10.5E\n", vector1->tempo, vector1->posizione, vector1->velocita, H_oscillatore(*vector1));
     }
 
-    euleroImplicitoOscillatore(vector2, 250, 10000, plot_E_implicito);
-    euleroTrapeziOscillatore(vector3, 250, 10000, plot_Trapezio);
+    for (size_t j = 0; j < N; j++) {
+        mk4(vector4, h, F, NULL);
+        fprintf(plot_E_mk4, "%10.5E %10.5E %10.5E %10.5E\n", vector4->tempo, vector4->posizione, vector4->velocita, H_oscillatore(*vector4));
+    }
+
+    euleroImplicitoOscillatore(vector2, T, N, plot_E_implicito);
+    euleroTrapeziOscillatore(vector3, T, N, plot_Trapezio);
 
     return 0;
 }
